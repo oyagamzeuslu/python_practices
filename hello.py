400eb4753a3b1f94c9882f0aae98d683e4f6293e
10505715 <gamze.uslu@yandex.com>
2021-09-06 13:59:23 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,27 @@
+students = []
+add_confirm = "y"
+user_input = ""
+
+while add_confirm == "y":
+    add_confirm = input("Do you want to add a student? Enter y/n ")
+    if add_confirm == "n":
+        print("add process completed")
+        break
+    
+    user_input = input("enter student name: ")
+
+    for s in students:
+        if s == user_input:
+            print(user_input, " is already a student")
+            break
+        
+    else:
+        students.append(user_input)
+        print(user_input, "is now added")
+        print(students)
+
+
+
+
+
 
